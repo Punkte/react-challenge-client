b9dcdfb550a8f04ae97890d09f2b38d55d1dd088
PierreTurnbull <pierre.turnbull42@gmail.com>
2019-11-12 12:14:19 +0100

    Added empty case.

--- src/components/atoms/StudentList/StudentList.tsx
+++ src/components/atoms/StudentList/StudentList.tsx
@@ -15,6 +15,7 @@ interface Props {
 const StyledWrapper = styled.div`
   border-radius: 4px;
   overflow: hidden;
+  background-color: white;
 `;
 
 const StyledBorder = styled.div`
@@ -22,19 +23,30 @@ const StyledBorder = styled.div`
   height: 11px;
 `;
 
+const StyledEmpty = styled.div`
+  padding: 15px;
+  text-align: center;
+  color: #4E4E56;
+  font-size: 12px;
+  font-weight: 500;
+`;
+
 export const StudentList: React.FC<Props> = (props) => {
   return (
     <StyledWrapper>
       <StyledBorder />
-      {props.students.map((student, index) => (
-        <StudentCard
-          key={index}
-          backgroundColor={index % 2 === 0 ? '#F3F3F3' : ''}
-          firstName={student.firstName}
-          lastName={student.lastName}
-          specialties={student.specialties}
-        ></StudentCard>
-      ))}
+      {props.students.length
+        ? props.students.map((student, index) => (
+          <StudentCard
+            key={index}
+            backgroundColor={index % 2 === 0 ? '#F3F3F3' : ''}
+            firstName={student.firstName}
+            lastName={student.lastName}
+            specialties={student.specialties}
+          ></StudentCard>
+        ))
+        : <StyledEmpty>Aucun étudiant ne correspond à la recherche.</StyledEmpty>
+      }
       <StyledBorder />
     </StyledWrapper>
   );
