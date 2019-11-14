448fef12787d5dcc2c4213f3b64d8658fb0b13a4
PierreTurnbull <pierre.turnbull42@gmail.com>
2019-11-14 17:03:31 +0100

    Added filter component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import LoginView from 'components/pages/LoginView';
--- src/models/Student.model.tsx
+++ src/models/Student.model.tsx
@@ -1,4 +1,4 @@
-export interface Student {
+export interface StudentModel {
   firstName: string;
   lastName: string;
   skills: string[];
